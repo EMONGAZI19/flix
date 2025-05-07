66f679d511ad4c5185161e92ac58f45fd384097f
EMONGAZI19 <afranemon84@gmail.com>
2025-05-08 00:46:55 +0600

    Create movies.js

---
+++ movies.js
@@ -0,0 +1,40 @@
+const movies = {
+  latestMovies: [
+    { title: 'Movie 1', image: 'movie1.jpg' },
+    { title: 'Movie 2', image: 'movie2.jpg' },
+    { title: 'Movie 3', image: 'movie3.jpg' },
+    { title: 'Movie 4', image: 'movie4.jpg' },
+    { title: 'Movie 5', image: 'movie5.jpg' },
+    { title: 'Movie 6', image: 'movie6.jpg' },
+    { title: 'Movie 7', image: 'movie7.jpg' },
+    { title: 'Movie 8', image: 'movie8.jpg' },
+    { title: 'Movie 9', image: 'movie9.jpg' },
+    { title: 'Movie 10', image: 'movie10.jpg' },
+    { title: 'Movie 11', image: 'movie11.jpg' },
+    { title: 'Movie 12', image: 'movie12.jpg' },
+    { title: 'Movie 13', image: 'movie13.jpg' },
+    { title: 'Movie 14', image: 'movie14.jpg' },
+    { title: 'Movie 15', image: 'movie15.jpg' }
+  ],
+  banglaMovies: [
+    { title: 'Bangla Movie 1', image: 'bangla1.jpg' },
+    { title: 'Bangla Movie 2', image: 'bangla2.jpg' },
+    // Add more movies here...
+  ],
+  hindiDubbedMovies: [
+    { title: 'Hindi Movie 1', image: 'hindi1.jpg' },
+    // Add more movies here...
+  ],
+  hollywoodMovies: [
+    { title: 'Hollywood Movie 1', image: 'hollywood1.jpg' },
+    // Add more movies here...
+  ],
+  banglaDubbedMovies: [
+    { title: 'Bangla Dubbed Movie 1', image: 'bangladubbed1.jpg' },
+    // Add more movies here...
+  ],
+  animeMovies: [
+    { title: 'Anime Movie 1', image: 'anime1.jpg' },
+    // Add more movies here...
+  ]
+};
